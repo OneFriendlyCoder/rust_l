50abb5ce29b15d167fa9f68478ee908b72398582
onefriendlycoder <abhyanand311099@gmail.com>
2025-08-01 12:12:42 +0530

    default implementations with functions

--- chapter10/generic_types_and_traits/src/aggregator.rs
+++ chapter10/generic_types_and_traits/src/aggregator.rs
@@ -1,11 +1,23 @@
 // traits : A type's behaviour consits of the methods we can call on that type. Different types share the same behaviour if we can call the same methods on all of those types.
 // traits are a way to group method signatures together to define a set of behaviours necessary to accomplish some purpose
 
-pub trait Summary {                 // pub for other crates to use this trait
-    fn summarize(&self) -> String;          //the function signature, the body would be implemented by each type that uses this trait
-    // we can have muliple function signature for a single trait
+// pub trait Summary {                 // pub for other crates to use this trait
+//     fn summarize(&self) -> String;          //the function signature, the body would be implemented by each type that uses this trait
+//     // we can have muliple function signature for a single trait
 
+// }
+
+
+//default behaviour for some or all methods in a trait instead of requiring implementations for all methods on every type
+pub trait Summary{
+
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String{
+        format!("Read more from : {}", self.summarize_author())
+    }
 }
+
 
 pub struct NewsArticle {
     pub headline: String,
@@ -14,24 +26,37 @@ pub struct NewsArticle {
     pub content: String,
 }
 
+//To use a default implementation to summarize instances of NewsArticle
+// impl Summary for NewsArticle {}              // using this again is giving me error as there are 2 implementations of Summary for NewsArticle, we need only one, this is for default implementations
+
 impl Summary for NewsArticle{
-    fn summarize(&self) -> String{
-        format!("{}, by {} at ({})", self.headline, self.author, self.location)
+    // fn summarize(&self) -> String{
+    //     format!("{}, by {} at ({})", self.headline, self.author, self.location)
+    // }
+
+    fn summarize_author(&self) -> String{
+        format!("@{}", self.author)
     }
 }
 
-pub struct SocialPost{
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub repost: bool,
-}
-
-impl Summary for SocialPost{
-    fn summarize(&self) -> String{
-        format!("{}: {}", self.username, self.content)
-    }
-}
+// pub struct SocialPost{
+//     pub username: String,
+//     pub content: String,
+//     pub reply: bool,
+//     pub repost: bool,
+// }
+// impl Summary for SocialPost {}
+// // impl Summary for SocialPost{
+// //     fn summarize(&self) -> String{
+// //         format!("{}: {}", self.username, self.content)
+// //     }
+// // }
 
 
 //fn summarize(&self) -> String;  => self refers to the instance of any data type that is using the trait, &self is an immutable pointer
+
+//Note : 
+// One restriction to note is that we can implement a trait on a type only if either the trait or the type, or both, are local to our crate 
+
+
+//Default implementations can call other methods in the same trait. Even if those methods do not have a default implementaiton
--- chapter10/generic_types_and_traits/src/main.rs
+++ chapter10/generic_types_and_traits/src/main.rs
@@ -1,12 +1,24 @@
 mod aggregator;                 //declare that there is a module named aggregator and its code is in aggregator.rs or aggregator/mod.rs
-use aggregator::{SocialPost, Summary};
-
+// use aggregator::{SocialPost, Summary};
+// use aggregator::{SocialPost,NewsArticle, Summary};
+use aggregator::{NewsArticle, Summary};
 fn main(){
-    let post = SocialPost{
-        username: String::from("Abhyanand"),
-        content: String::from("This content is so crazy"),
-        reply: false,
-        repost: false,
+    // let post = SocialPost{
+    //     username: String::from("Abhyanand"),
+    //     content: String::from("This content is so crazy"),
+    //     reply: false,
+    //     repost: false,
+    // };
+    // println!("1 new social post: {}", post.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
     };
-    println!("1 new social post: {}", post.summarize());
+    println!("New article available! {}", article.summarize());
 }
